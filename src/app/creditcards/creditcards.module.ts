2392c38bf84c98e88fc643fb275b1de5d4c3053d
PiaLember <pia.lember@gmail.com>
2024-05-23 10:44:22 +0300

    Mat Table and Read Functionality

--- src/app/creditcards/creditcards.module.ts
+++ src/app/creditcards/creditcards.module.ts
@@ -7,6 +7,12 @@ import { AddComponent } from './add/add.component';
 import { ViewComponent } from './view/view.component';
 import { EditComponent } from './edit/edit.component';
 import { DeleteComponent } from './delete/delete.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 @NgModule({
@@ -19,7 +25,12 @@ import { DeleteComponent } from './delete/delete.component';
   ],
   imports: [
     CommonModule,
-    CreditcardsRoutingModule
+    CreditcardsRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatIconModule
   ]
 })
 export class CreditcardsModule { }
