b854062c2ba495c57344602f4ce0c58c3a81579c
PiaLember <pia.lember@gmail.com>
2024-05-24 20:21:38 +0300

    Service Implementation

--- src/app/services/creditcards.service.ts
+++ src/app/services/creditcards.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CreditCard } from '../models/credit-card';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreditcardsService {
 
-  constructor() { }
+  private apiUrl = "";
+  constructor(private httpClient: HttpClient) { }
+
+  //CRUD Functionality
+
+  createCreditCard(creditCard: CreditCard): Observable<CreditCard> {
+    return this.httpClient.post<CreditCard>(this.apiUrl, creditCard);
+  }
+
+  getCreditCards(): Observable<CreditCard[]>{
+    return this.httpClient.get<CreditCard[]>(this.apiUrl);
+  }
+
+  getCreditCardById(id: Number): Observable<CreditCard>{
+    const url = `${this.apiUrl}/${id}`;
+    return this.httpClient.get<CreditCard>(url);
+  }
+
 }
