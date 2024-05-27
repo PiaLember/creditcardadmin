66d84a9eb66bfe164960369e81d2453656768f8f
PiaLember <pia.lember@gmail.com>
2024-05-27 12:54:50 +0300

    Integration with API

--- src/app/services/creditcards.service.ts
+++ src/app/services/creditcards.service.ts
@@ -8,22 +8,38 @@ import { Observable } from 'rxjs';
 })
 export class CreditcardsService {
 
-  private apiUrl = "";
+  private apiUrl = "http://localhost:3000/creditcards";
+
   constructor(private httpClient: HttpClient) { }
 
-  //CRUD Functionality
+  // CRUD Functionality 
 
+  // Create New Credit Card
   createCreditCard(creditCard: CreditCard): Observable<CreditCard> {
     return this.httpClient.post<CreditCard>(this.apiUrl, creditCard);
   }
 
+  // Get All Credit Cards
   getCreditCards(): Observable<CreditCard[]>{
     return this.httpClient.get<CreditCard[]>(this.apiUrl);
   }
 
-  getCreditCardById(id: Number): Observable<CreditCard>{
+  // Get Specific Credit Card
+  getCreditCardById(id: Number): Observable<CreditCard> {
     const url = `${this.apiUrl}/${id}`;
     return this.httpClient.get<CreditCard>(url);
+  }
+
+  // Update Functionality 
+  updateCreditCard(creditCard: CreditCard): Observable<CreditCard> {
+    const url = `${this.apiUrl}/${creditCard.id}`;
+    return this.httpClient.put<CreditCard>(url, creditCard);
+  }
+
+  // Delete Functionality
+  deleteCreditCard(id: Number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.httpClient.delete<void>(url);
   }
 
 }
